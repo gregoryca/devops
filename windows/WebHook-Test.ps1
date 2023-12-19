4141a5a918f9275cfa2b63a4a6de1da93661d94f
Gregory Craane <gregoryca@users.noreply.github.com>
2023-12-19 19:17:56 +0100

    Added a webhook to trigger github actions pipeline

---
+++ windows/WebHook-Test.ps1
@@ -0,0 +1,8 @@
+$trigger = 1
+
+if ($trigger -eq 1)
+{
+    $httpBody = @{"event_type"="upgrade_traefik"} | ConvertTo-Json
+
+    $null = Invoke-WebRequest -Uri 'https://api.github.com/repos/gregoryca/traefik/dispatches' -Method Post -Body $httpBody -Headers @{'Accept' = 'application/vnd.github.everest-preview+json'; 'Authorization' = 'Bearer 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'} -ContentType "application/json"
+}
