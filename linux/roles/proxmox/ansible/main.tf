a2b24e63cb0a345e4547bf83712c05dfca3ff43e
gregoryca <aquiles.craane@gapex-migrations.net>
2025-06-10 11:54:58 +0200

    moved resource to its module

--- linux/roles/proxmox/ansible/main.tf
+++ linux/roles/proxmox/ansible/main.tf
@@ -43,6 +43,24 @@ variable "auth_key" {
     description = ""
 }
 
+resource "ansible_group" "web" {
+  name     = "web"
+  children = ["home"]
+
+  # Group variables that will apply to the children hosts.
+  variables = {
+    ansible_ssh_user             = var.ansible_ssh_user
+    ansible_ssh_private_key_file = var.ansible_ssh_private_key_file
+    ansible_sudo_pass            = var.ansible_sudo_pass
+    docker_stack_name            = var.docker_stack_name
+    docker_image                 = var.docker_image
+    state                        = var.state
+    sources                      = var.sources
+    hostname                     = var.hostname
+    auth_key                     = var.auth_key
+  }
+}
+
 resource "ansible_host" "vm" {          #### ansible host details
   name   = "terraform-ansible-proxmox-vm"
   groups = ["home"]
--- linux/roles/proxmox/main.tf
+++ linux/roles/proxmox/main.tf
@@ -6,25 +6,6 @@ module "domain" {
   source = "./transip"
 }
 
-resource "ansible_group" "web" {
-  name     = "web"
-  children = ["/main"]
-
-  # Group variables that apply to all the children hosts.
-  variables = {
-    ansible_ssh_private_key_file = var.ssh_private_key_path
-    ansible_ssh_user             = var.ansible_ssh_user
-    ansible_ssh_private_key_file = var.ansible_ssh_private_key_file
-    ansible_sudo_pass            = var.ansible_sudo_pass
-    docker_stack_name            = var.docker_stack_name
-    docker_image                 = var.docker_image
-    state                        = var.state
-    sources                      = var.sources
-    hostname                     = var.hostname
-    auth_key                     = var.auth_key
-  }
-}
-
 module "ansible" {
   source                       = "./ansible"
   ansible_ssh_user             = var.ansible_ssh_user
